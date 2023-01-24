d17c3c71fd464483df93a6ec496b3efe55688344
Sakina251 <92969280+Sakina251@users.noreply.github.com>
2023-01-24 09:39:25 +0530

    Update exp3.java

--- exp3.java
+++ exp3.java
@@ -1,9 +1,8 @@
-// Java code to illustrate
-// System.out.println();
+
 
 import java.io.*;
 
-class GFG {
+class Hello {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
